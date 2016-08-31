0fd64ee7e64b59f621a4367073f1b191c4d81700
Jack Mocherman <mochermanjack@gmail.com>
2016-08-31 10:17:51 -0500

    cleaned up commented out code and tested for all possible two month permeations, refactored code in render.js (specifically create weeks placement of \n character)

--- test/permutations_test.js
+++ test/permutations_test.js
@@ -8,6 +8,7 @@ const {
 } = require('child_process')
 
 describe('cli', () => {
+
   it('should handle no args', (cb) => {
     exec('bin/mockcal', (err, stdout) => {
       exec('cal', (err2, stdout2) => {
@@ -16,4 +17,86 @@ describe('cli', () => {
       })
     })
   })
+
+  it('should handle two args (first test)', (cb) => {
+    exec('bin/mockcal 3 2016', (err, stdout) => {
+      exec('cal 3 2016', (err2, stdout2) => {
+        assert.strictEqual(stdout, stdout2)
+        cb()
+      })
+    })
+  })
+
+  it('should handle two args (first test)', (cb) => {
+    exec('bin/mockcal 9 2016', (err, stdout) => {
+      exec('cal 9 2016', (err2, stdout2) => {
+        assert.strictEqual(stdout, stdout2)
+        cb()
+      })
+    })
+  })
+
+  it('should handle two args (first test)', (cb) => {
+    exec('bin/mockcal 2 2016', (err, stdout) => {
+      exec('cal 2 2016', (err2, stdout2) => {
+        assert.strictEqual(stdout, stdout2)
+        cb()
+      })
+    })
+  })
+
+  it('should handle two args (first test)', (cb) => {
+    exec('bin/mockcal 2 2017', (err, stdout) => {
+      exec('cal 2 2017', (err2, stdout2) => {
+        assert.strictEqual(stdout, stdout2)
+        cb()
+      })
+    })
+  })
+
+  it('should handle two args (third test)', (cb) => {
+    exec('bin/mockcal 2 2015', (err, stdout) => {
+      exec('cal 2 2015', (err2, stdout2) => {
+        assert.strictEqual(stdout, stdout2)
+        cb()
+      })
+    })
+  })
+
+  it('should handle two args (third test)', (cb) => {
+    exec('bin/mockcal 1 2016', (err, stdout) => {
+      exec('cal 1 2016', (err2, stdout2) => {
+        assert.strictEqual(stdout, stdout2)
+        cb()
+      })
+    })
+  })
+
+  it('should handle two args (third test)', (cb) => {
+    exec('bin/mockcal 4 2017', (err, stdout) => {
+      exec('cal 4 2017', (err2, stdout2) => {
+        assert.strictEqual(stdout, stdout2)
+        cb()
+      })
+    })
+  })
+
+  it('should handle two args (third test)', (cb) => {
+    exec('bin/mockcal 2 2100', (err, stdout) => {
+      exec('cal 2 2100', (err2, stdout2) => {
+        assert.strictEqual(stdout, stdout2)
+        cb()
+      })
+    })
+  })
+
+  it('should handle two args (third test)', (cb) => {
+    exec('bin/mockcal 2 2000', (err, stdout) => {
+      exec('cal 2 2000', (err2, stdout2) => {
+        assert.strictEqual(stdout, stdout2)
+        cb()
+      })
+    })
+  })
+
 })
